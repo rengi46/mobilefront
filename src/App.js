1f8d61171efe8fd1aa8ca7bfafbfe40683ff55b3
Roger <rpuigdemasa@phygitalbcn.com>
2023-11-14 12:12:12 +0100

    add images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
+import gatos from "./images/gato.jpg";
 
 const socket = io("http://localhost:3001");
 function App() {
@@ -15,11 +16,25 @@ function App() {
 
   return (
     <div className="App">
-     <h2>{conect ? "conectado":"no conectado"}</h2>
-      <input type="color" onChange={(e) => {
+     {/* <h2>{conect ? "conectado":"no conectado"}</h2> */}
+      {/* <input type="color" onChange={(e) => {
         socket.emit("colorChange", e.target.value);
       }
-      }/>
+      }/> */}
+      <div className='selectVideo'>
+        <img src={gatos} onClick={() => {
+          socket.emit("videoChange", "gatos");
+        }}/>
+        <img src={gatos} onClick={() => {
+          socket.emit("videoChange", "perros");
+        }}/>
+        <img src={gatos} onClick={() => {
+          socket.emit("videoChange", "niños");
+        }}/>
+        <img src={gatos} onClick={() => {
+          socket.emit("videoChange", "moments");
+        }}/>
+      </div>
     </div>
   );
 }
